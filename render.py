6fdcdd8f3ea65d53aeb3eda785ca6c7285c80ea3
dk949 <56653556+dk949@users.noreply.github.com>
2021-05-07 22:29:50 +0100

    Added propper logging for 'WHAT IS THIS'

--- render.py
+++ render.py
@@ -258,6 +258,8 @@ class Render:
                 probe_all = ffmpeg.probe('{}'.format(file_name), cmd=self.ffprobe_path)
             except Exception:
                 self.logger.warning("WHAT IS THIS")  # FIXME: @JUL14N this one's for you buddy:)
+                stderr = e.stderr.decode('ascii').replace('\n', '\n\t')
+                self._logger.warning(f"ffprobe stderr:\n\t{stderr}")
             if stream == 'v':
                 self.logger.debug("probe result -\n" + json.dumps(probe_all, indent=2))
             else:  # TODO: this is here to prove that we unnecessarily call has_stream twice
